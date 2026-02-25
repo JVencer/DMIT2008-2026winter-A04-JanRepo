884cafae6e2650891db2dc8a5e0ae6ff39790d71
JVencer <jvencer1@nait.ca>
2026-02-25 10:10:06 -0700

    update

--- 04 - react intro/examples/02 - react + nextjs components/student/components-example/app/page.js
+++ 04 - react intro/examples/02 - react + nextjs components/student/components-example/app/page.js
@@ -1,11 +1,16 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
+import Hello from "@/components/Hello.js"
+import NewConcept from "@/components/NewConcept";
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      
+      <div className={styles.page}>
+        <main className={styles.main}>
+          <h1>My React App hehe</h1>
+          <Hello />
+        </main>
+      </div>
       </main>
     </div>
   );
